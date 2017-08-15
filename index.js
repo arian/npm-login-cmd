0fa588c69405d280f34946b159b61adaedc3051f
Arian Stolwijk <stolwijk.arian@gmail.com>
2017-08-15 09:48:44 +0200

    Exit if one of the parameters are missing and inherit stdout/stderr

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
 
 const child_process = require("child_process");
-const child = child_process.spawn("npm", ["login", "-q"]);
 
 const username = process.env.NPM_USER;
 const password = process.env.NPM_PASS;
 const email = process.env.NPM_EMAIL;
 
-child.stdout.on("data", data => {
-  const string = data.toString();
-  if (string.match(/username/i)) {
-    child.stdin.write(username + "\n");
-  } else if (string.match(/password/i)) {
-    child.stdin.write(password + "\n");
-  } else if (string.match(/email/i)) {
-    child.stdin.write(email + "\n");
-  }
-  console.log(string);
+if (!username) {
+  console.error("Please set the NPM_USER environment variable");
+  process.exit(1);
+}
+
+if (!password) {
+  console.error("Please set the NPM_PASS environment variable");
+  process.exit(1);
+}
+
+if (!email) {
+  console.error("Please set the NPM_EMAIL environment variable");
+  process.exit(1);
+}
+
+const child = child_process.spawn("npm", ["login", "-q"], {
+  stdio: ["pipe", "inherit", "inherit"]
 });
+
+child.stdin.write(username + "\n");
+child.stdin.write(password + "\n");
+child.stdin.write(email + "\n");
+child.stdin.end();
