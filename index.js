7dbe20065545b2d34b343f3bd4c7b777b37e343f
Marc d'Entremont <marc.dentremont@bluescape.com>
2019-03-08 11:26:30 -0800

    Add arg support, eslint and prettier

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 #!/usr/bin/env node
 
-const child_process = require("child_process");
+/* eslint-disable no-console */
+const childProcess = require('child_process');
+const args = require('commander');
 
-const username = process.env.NPM_USER;
-const password = process.env.NPM_PASS;
-const email = process.env.NPM_EMAIL;
+args
+  .version('1.1.0')
+  .option('-e, --email', 'Email address')
+  .option('-u, --username', 'Username')
+  .option('-p, --password', 'Password')
+  .parse(process.argv);
+
+const username = args.repository || process.env.NPM_USER;
+const password = args.password || process.env.NPM_PASS;
+const email = args.username || process.env.NPM_EMAIL;
 
 if (!username) {
-  console.error("Please set the NPM_USER environment variable");
+  console.error('Please pass arg username or set the NPM_USER environment variable');
   process.exit(1);
 }
 
 if (!password) {
-  console.error("Please set the NPM_PASS environment variable");
+  console.error('Please pass arg password or set the NPM_PASS environment variable');
   process.exit(1);
 }
 
 if (!email) {
-  console.error("Please set the NPM_EMAIL environment variable");
+  console.error('Please pass arg email or set the NPM_EMAIL environment variable');
   process.exit(1);
 }
 
-const child = child_process.spawn("npm", ["login", "-q"], {
-  stdio: ["pipe", "pipe", "inherit"]
+const child = childProcess.spawn('npm', ['login', '-q'], {
+  stdio: ['pipe', 'pipe', 'inherit'],
 });
 
-child.stdout.on("data", d => {
+child.stdout.on('data', d => {
   const data = d.toString();
-  process.stdout.write(d + "\n");
+  process.stdout.write(`${d}\n`);
   if (data.match(/username/i)) {
-    child.stdin.write(username + "\n");
+    child.stdin.write(`${username}\n`);
   } else if (data.match(/password/i)) {
-    child.stdin.write(password + "\n");
+    child.stdin.write(`${password}\n`);
   } else if (data.match(/email/i)) {
-    child.stdin.write(email + "\n");
+    child.stdin.write(`${email}\n`);
   } else if (data.match(/logged in as/i)) {
     child.stdin.end();
   }
